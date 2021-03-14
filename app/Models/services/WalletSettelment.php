ada9ce379afb8a54be1f2cc7833cf9dff0bf741d
gurubachan Singh <way2gurubachan@gmail.com>
2021-03-15 05:29:12 +0530

    15032021 - Wallet Payout option implimentation started

---
+++ app/Models/services/WalletSettelment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\services;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WalletSettelment extends Model
+{
+    use HasFactory;
+}
